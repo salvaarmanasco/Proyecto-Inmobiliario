f001a80ac96a16c7819564e854736f2f96f7ccc6
Facuzanan88 <facundozanandrea@gmail.com>
2023-01-21 15:32:44 -0300

    fear(api/src): creacion de DB

---
+++ api/api/dist/db/config.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sequelize_typescript_1 = require("sequelize-typescript");
+const todos_1 = require("../models/todos");
+const connection = new sequelize_typescript_1.Sequelize({
+    dialect: "mysql",
+    host: "localhost",
+    username: "root",
+    password: "Your_mysql_password",
+    database: "todo",
+    logging: false,
+    models: [todos_1.Todos],
+});
+exports.default = connection;
