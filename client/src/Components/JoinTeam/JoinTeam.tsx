66800887a2440311157b351cf9f916fcb86d752a
Santirbe98 <santirbe@gmail.com>
2023-02-01 18:56:15 -0300

    feat(JoinTeam)

--- client/src/Components/JoinTeam/JoinTeam.tsx
+++ client/src/Components/JoinTeam/JoinTeam.tsx
@@ -1,7 +1,60 @@
-import { Box, Text, Center } from "@chakra-ui/react";
+import {
+  Container,
+  SimpleGrid,
+  Image,
+  Flex,
+  Heading,
+  Text,
+  Stack,
+  StackDivider,
+  Icon,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { ReactElement } from "react";
+import Contact from "../Contact/Contact";
 
-const JoinTeam = () => {
-  return <Center>JoinTeam</Center>;
+interface FeatureProps {
+  text: string;
+  iconBg: string;
+  icon?: ReactElement;
+}
+
+const Feature = ({ text, icon, iconBg }: FeatureProps) => {
+  return (
+    <Stack direction={"row"} align={"center"}>
+      <Flex
+        w={8}
+        h={8}
+        align={"center"}
+        justify={"center"}
+        rounded={"full"}
+        bg={iconBg}
+      >
+        {icon}
+      </Flex>
+      <Text fontWeight={600}>{text}</Text>
+    </Stack>
+  );
 };
 
-export default JoinTeam;
+export default function SplitWithImage() {
+  return (
+    <Container maxW={"5xl"} py={12}>
+      <SimpleGrid columns={{ base: 1, md: 1 }} spacing={10}>
+        <Stack spacing={4}>
+          <Stack
+            spacing={4}
+            borderRadius="md"
+            divider={
+              <StackDivider
+                borderColor={useColorModeValue("gray.100", "gray.700")}
+              />
+            }
+          >
+            <Contact />
+          </Stack>
+        </Stack>
+      </SimpleGrid>
+    </Container>
+  );
+}
