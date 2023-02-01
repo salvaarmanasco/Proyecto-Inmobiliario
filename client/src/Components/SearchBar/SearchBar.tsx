55787439c618ec0896d253af1e1c4dd5b81b42f4
Santirbe98 <santirbe@gmail.com>
2023-02-01 18:30:19 -0300

    feat(Searchbar)

--- client/src/Components/SearchBar/SearchBar.tsx
+++ client/src/Components/SearchBar/SearchBar.tsx
@@ -1,9 +1,25 @@
-import { Box, Text, Heading } from "@chakra-ui/react";
+import { Box, Stack, Input, Select } from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 
 const SearchBar = () => {
   return (
     <Box textAlign="center" fontSize="xl">
-      SearchBar
+      <Stack
+        justifyContent="center"
+        align="center"
+        direction={{ base: "column", sm: "column", md: "row" }}
+      >
+        <Select
+          icon={<ChevronDownIcon />}
+          placeholder="Tipo de operacion"
+          w="180px"
+        >
+          <option>Venta</option>
+          <option>Alquiler</option>
+        </Select>
+
+        <Input placeholder="Busqueda" size="md" width="md" />
+      </Stack>
     </Box>
   );
 };
