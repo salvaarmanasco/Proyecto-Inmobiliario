aee69ad6882aa1071c0102f546eee1a63832290b
Santirbe98 <santirbe@gmail.com>
2023-02-15 19:35:15 -0300

    feat(React-Router-Dom)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,25 @@
-import { ChakraProvider, Box, theme } from "@chakra-ui/react";
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import { Switch, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar/NavBar";
-import Tittle from "./Components/Tittle/Tittle";
-import SearchBar from "./Components/SearchBar/SearchBar";
-import Cards from "./Components/Cards/Cards";
-import Carrousel from "./Components/Carrousel/Carrousel";
-import Newsletter from "./Components/Newsletter/Newsletter";
-import JoinTeam from "./Components/JoinTeam/JoinTeam";
-import Counter from "./Components/Counter/Counter";
 import Footer from "./Components/Footer/Footer";
+import LandingPage from "./Components/LandingPage/LandingPage";
+import About from "./Components/About/About";
+import Franchise from "./Components/Franchise/Franchise";
+import Page404 from "./Components/Page404/Page404";
+import PropertiesPage from "./Components/PropertiesPage/PropertiesPage";
+import CardDetails from "./Components/CardsDetail/CardDetails";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <NavBar />
-    <Box textAlign="center" minHeight="100vh" overflow="hidden">
-      <Tittle />
-      <SearchBar />
-      <Carrousel />
-      <Cards />
-      <Newsletter />
-      <JoinTeam />
-      <Counter />
-    </Box>
+    <Switch>
+      <Route exact path="/" component={LandingPage} />
+      <Route exact path="/properties" component={PropertiesPage} />
+      <Route exact path="/properties/:id" component={CardDetails} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/franchise" component={Franchise} />
+      <Route path="*" component={Page404} />
+    </Switch>
     <Footer />
   </ChakraProvider>
 );
