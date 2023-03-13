fa2f8bcf2ec0e5775d52caf5933ccf74f7ad7cb0
Santirbe98 <santirbe@gmail.com>
2023-03-13 19:05:58 -0300

    feat(tables)

--- client/src/Components/Counter/Counter.tsx
+++ client/src/Components/Counter/Counter.tsx
@@ -1,6 +1,30 @@
 import { Box, Text, Center } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchCategory,
+  fetchCondition,
+  fetchCountry,
+  fetchGarden,
+  fetchServices,
+  fetchState,
+} from "../../Redux/reducer/Tables";
+import { ThunkDispatch } from "redux-thunk";
+import { RootState } from "../../Redux/store";
 
 const Counter = () => {
+  const tables = useSelector((state: RootState) => state.tables);
+  const dispatch: ThunkDispatch<RootState, undefined, any> = useDispatch();
+  console.log(tables);
+
+  useEffect(() => {
+    dispatch(fetchCategory());
+    dispatch(fetchCondition());
+    dispatch(fetchCountry());
+    dispatch(fetchGarden());
+    dispatch(fetchServices());
+    dispatch(fetchState());
+  }, [dispatch]);
   return <Center>Counter</Center>;
 };
 
--- client/src/Redux/store.tsx
+++ client/src/Redux/store.tsx
@@ -1,13 +1,16 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { propertiesReducer, PropertiesState } from "./reducer/Properties";
+import { tablesReducer, TablesState } from "./reducer/Tables";
 
 export interface RootState {
   properties: PropertiesState;
+  tables: TablesState;
   // Aquí puedes agregar otras propiedades del estado si las tienes
 }
 
 const rootReducer = combineReducers({
   properties: propertiesReducer,
+  tables: tablesReducer,
   // Aquí puedes agregar otros reducers si los tienes
 });
 
