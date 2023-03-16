8c03f7b6c3e108453c2c283356dc7f27871e4766
Santirbe98 <santirbe@gmail.com>
2023-03-16 19:25:00 -0300

    ARREGLO

--- client/src/Redux/reducer/Properties.tsx
+++ client/src/Redux/reducer/Properties.tsx
@@ -11,35 +11,6 @@ export interface PropertiesState {
   error: string | null;
 }
 
-// Define la interfaz para cada propiedad
-export interface Property {
-  id: number;
-  name: string;
-  antiquity: number;
-  address: string;
-  title: string;
-  bedrooms: number;
-  bathrooms: number;
-  environments: number;
-  pool: boolean;
-  elevator: boolean;
-  floor_th: number;
-  orientation: string;
-  m2_totals: number;
-  m2_covered: number;
-  garage: boolean;
-  amenities: boolean;
-  description: string;
-  furnished: boolean;
-  balcony: boolean;
-  sign: boolean;
-  lat: number;
-  long: number;
-  price: number;
-  zone: string;
-  deleted: boolean;
-}
-
 // Crea una acción asíncrona de Redux con createAsyncThunk
 export const fetchProperties = createAsyncThunk(
   "properties/fetchProperties", // nombre de la acción
