bdb1a3c9ed3f824b0ecb3c9919fce7837f059c76
juanetem3n <juanmanuelalcaide@gmail.com>
2023-03-19 15:56:18 -0300

    counterFeatjuanete

--- client/src/Components/Counter/Counter.tsx
+++ client/src/Components/Counter/Counter.tsx
@@ -1,30 +1,161 @@
-import { Box, Text, Center } from "@chakra-ui/react";
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  fetchCategory,
-  fetchCondition,
-  fetchCountry,
-  fetchGarden,
-  fetchServices,
-  fetchState,
-} from "../../Redux/reducer/Tables";
-import { ThunkDispatch } from "redux-thunk";
-import { RootState } from "../../Redux/store";
 
-const Counter = () => {
-  const tables = useSelector((state: RootState) => state.tables);
-  const dispatch: ThunkDispatch<RootState, undefined, any> = useDispatch();
+//Este codigo implementa  funcion spin y usa estados para que el spin sea independiente en cada componente
 
-  useEffect(() => {
-    dispatch(fetchCategory());
-    dispatch(fetchCondition());
-    dispatch(fetchCountry());
-    dispatch(fetchGarden());
-    dispatch(fetchServices());
-    dispatch(fetchState());
-  }, [dispatch]);
-  return <Center>Counter</Center>;
+import { useState } from "react";
+import { Card, CardBody, Center, SimpleGrid, Text } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faKey, faUserTie, faAward } from "@fortawesome/free-solid-svg-icons";
+
+interface FontAwesomeIconWithSpinProps {
+  icon: any;
+  size: any;
+}
+
+const FontAwesomeIconWithSpin = ({
+  icon,
+  size,
+}: FontAwesomeIconWithSpinProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+  
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+  
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+  
+  return (
+    <Center
+    onMouseEnter={handleMouseEnter}
+    onMouseLeave={handleMouseLeave}
+    style={{ cursor: "pointer" }}
+    >
+      <FontAwesomeIcon
+        icon={icon}
+        size={size}
+        spin={isHovered}
+        style={{ paddingTop: "1rem", paddingBottom: "1rem" }}
+        />
+    </Center>
+  );
 };
 
-export default Counter;
+export default function Counter() {
+  return (
+    <Center my={5}>
+      <SimpleGrid
+        gap={4}
+        columns={3}
+        alignContent="center"
+        alignItems="flex-start"
+        justifyContent="center"
+        >
+        <Card>
+          <CardBody>
+            <Text textAlign="center">Propiedades Vendidas</Text>
+            <FontAwesomeIconWithSpin icon={faKey} size="3x" />
+            <Text textAlign="center" fontSize="3xl">
+              450
+            </Text>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            <Text textAlign="center">Asesores</Text>
+            <FontAwesomeIconWithSpin icon={faUserTie} size="3x" />
+            <Text textAlign="center" fontSize="3xl">
+              450
+            </Text>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            <Text textAlign="center">Años de franquicia</Text>
+            <FontAwesomeIconWithSpin icon={faAward} size="3x" />
+            <Text textAlign="center" fontSize="3xl">
+              450
+            </Text>
+          </CardBody>
+        </Card>
+      </SimpleGrid>
+    </Center>
+  );
+}
+// //este codigo dejo los iconos fijos. el siguiente implementa el splin.
+// import {
+//   Card,
+//   CardHeader,
+//   CardBody,
+//   CardFooter,
+//   Text,
+//   Box,
+//   Img,
+//   Flex,
+//   SimpleGrid,
+//   Center,
+//   Icon,
+// } from "@chakra-ui/react";
+
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faKey, faUserTie, faAward } from "@fortawesome/free-solid-svg-icons";
+
+// export default function Counter() {
+//   return (
+//     <Center my={5}>
+//       <SimpleGrid
+//         gap={4}
+//         columns={3}
+//         alignContent="center"
+//         alignItems="flex-start"
+//         justifyContent="center"
+//       >
+//         <Card>
+//           <CardBody>
+//             <Text textAlign="center">Propiedades Vendidas</Text>
+//             <Center>
+//               <FontAwesomeIcon
+//                 icon={faKey}
+//                 size="3x"
+//                 style={{ paddingTop: "1rem", paddingBottom: "1rem" }}
+//               />
+//             </Center>
+//             <Text textAlign="center" fontSize="3xl">
+//               450
+//             </Text>
+//           </CardBody>
+//         </Card>
+//         <Card>
+//           <CardBody>
+//             <Text textAlign="center">Asesores</Text>
+//             <Center>
+//               <FontAwesomeIcon
+//                 icon={faUserTie}
+//                 size="3x"
+//                 style={{ paddingTop: "1rem", paddingBottom: "1rem" }}
+//               />
+//             </Center>
+//             <Text textAlign="center" fontSize="3xl">
+//               450
+//             </Text>
+//           </CardBody>
+//         </Card>
+//         <Card>
+//           <CardBody>
+//             <Text textAlign="center">Años de franquicia</Text>
+//             <Center>
+//               <FontAwesomeIcon
+//                 icon={faAward}
+//                 size="3x"
+//                 style={{ paddingTop: "1rem", paddingBottom: "1rem" }}
+//               />
+//             </Center>
+//             <Text textAlign="center" fontSize="3xl">
+//               450
+//             </Text>
+//           </CardBody>
+//         </Card>
+//       </SimpleGrid>
+//     </Center>
+//   );
+// }
